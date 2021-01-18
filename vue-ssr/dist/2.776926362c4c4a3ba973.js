db344ef65d085d772f951d42799aa4c9272aff9b
qft <903419670@qq.com>
2021-01-18 15:26:49 +0800

    vue-ssr init

---
+++ vue-ssr/dist/2.776926362c4c4a3ba973.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{42:function(t,e,n){"use strict";n.r(e);var r={name:"Error404"},s=n(3),i=Object(s.a)(r,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("404 Not Page")])])}],!1,null,null,null);e.default=i.exports}}]);
+//# sourceMappingURL=2.776926362c4c4a3ba973.js.map
---
+++ vue-ssr/dist/3.ca8b89b7c8e02bb85b7b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{40:function(t,e,n){"use strict";n.r(e);var i={name:"AboutPage",metaInfo:{title:"关于"}},s=n(3),a=Object(s.a)(i,(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("About Page")])])}],!1,null,null,null);e.default=a.exports}}]);
+//# sourceMappingURL=3.ca8b89b7c8e02bb85b7b.js.map
